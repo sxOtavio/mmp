127e9cf651739e8b9b1b84b654bac33bc458f461
sxOtavio <ximenes.otavio@gmail.com>
2026-06-04 12:33:24 -0300

    fixing vercel buggs

--- src/app/userPage/page.js
+++ src/app/userPage/page.js
@@ -1,36 +1,10 @@
-'use client';
+import { Suspense } from "react";
+import UserPageClient from "@/components/UserPageClient";
 
-import { useState, useEffect } from "react";
-import { useSearchParams } from "next/navigation";
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
-import Categorias from "@/components/Categories";
-import PromoProducts from "@/components/PProducts";
-import Products from "@/components/promoProducts";
-import Footer from "@/components/Footer";
-import FiltredProducts from "@/components/FiltredProducts";
-
-export default function UserPage() {
-  const [selectedCategory, setSelectedCategory] = useState("Todos");
-  const [searchTerm, setSearchTerm] = useState("");
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    const search = searchParams.get("search");
-    if (search) {
-      setSearchTerm(search);
-    }
-  }, [searchParams]);
-
+export default function Page() {
   return (
-    <main>
-      <Header />
-      <Hero />
-      <Categorias selectedCategory={selectedCategory} onCategoryChange={setSelectedCategory} />
-      <FiltredProducts selectedCategory={selectedCategory} searchTerm={searchTerm} />
-      <Products />
-      <PromoProducts />
-      <Footer />
-    </main>
+    <Suspense fallback={<div>Carregando...</div>}>
+      <UserPageClient />
+    </Suspense>
   );
 }
---
+++ src/components/UserPageClient.jsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { useState, useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Categorias from "@/components/Categories";
+import PromoProducts from "@/components/PProducts";
+import Products from "@/components/promoProducts";
+import Footer from "@/components/Footer";
+import FiltredProducts from "@/components/FiltredProducts";
+
+export default function UserPageClient() {
+  const [selectedCategory, setSelectedCategory] = useState("Todos");
+  const [searchTerm, setSearchTerm] = useState("");
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const search = searchParams.get("search");
+    if (search) {
+      setSearchTerm(search);
+    }
+  }, [searchParams]);
+
+  return (
+    <main>
+      <Header />
+      <Hero />
+      <Categorias selectedCategory={selectedCategory} onCategoryChange={setSelectedCategory} />
+      <FiltredProducts selectedCategory={selectedCategory} searchTerm={searchTerm} />
+      <Products />
+      <PromoProducts />
+      <Footer />
+    </main>
+  );
+}
