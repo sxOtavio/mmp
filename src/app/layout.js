854d7b42a214bb225e9117434b67e37c7f8fec4c
sxOtavio <ximenes.otavio@gmail.com>
2026-06-11 13:59:48 -0300

    fixing vercel troblem

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,12 +21,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-BR">
-      <body>
-        <UserProvider>
-          {children}
-        </UserProvider>
-      </body>
+    <html
+      lang="pt-BR"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
