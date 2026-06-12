a9e18e933470b24ab9934253acbc66186066a9b7
sxOtavio <ximenes.otavio@gmail.com>
2026-06-12 03:32:11 -0300

    favicon correction

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
-'use client'; // 👈 IMPORTANTE
+// app/layout.js
+'use client';
 
 import { UserProvider } from '@/contexts/UserContext';
 import './globals.css';
@@ -6,6 +7,15 @@ import './globals.css';
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
+      <head>
+        <title>Supermercado Preferido</title>
+        <meta name="description" content="Ofertas imperdíveis e produtos de qualidade no Supermercado Preferido" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        
+        {/* Favicon tradicional */}
+        <link rel="icon" href="/favicon.png" sizes="any" />
+        
+      </head>
       <body>
         <UserProvider>
           {children}
